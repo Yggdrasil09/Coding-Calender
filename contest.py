a590be7663361c75540450251598587b87232d32
Yggdrasil09 <karthik99th.tnk@gmail.com>
2019-03-04 13:16:30 +0530

    Added codeforces link to json and necessary code for codeforces webpage

--- contest.py
+++ contest.py
@@ -12,7 +12,8 @@ for x in data:
     if x == "Codechef":
         headers = {'user-agent' : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Mobile Safari/537.36' }
         res = requests.get(data[x],headers=headers)
-    # print(res.text[:30000])
+    else:
+        res = requests.get(data[x])
     try:
         res.raise_for_status()
     except Exception as exc :
@@ -25,14 +26,24 @@ for x in data:
         index = elements[0].select('tbody tr')
         for tags in index:
             td = tags.select("td")
-            competitions.write('\nCompetion code : '+td[0].getText()+'\nContest Name: '+td[1].getText()+'\nStart time: '+td[2].getText()+'\nEnd time '+td[3].getText()+'\n\n')
+            competitions.write('\nCompetion code : '+td[0].getText()+'\nContest Name: '+td[1].getText()+'\nStart time: '+td[2].getText()+'\nEnd time '+td[3].getText())
+            competitions.write("\n--------------------------------------------------------------------------------------\n")
         competitions.write("Future Contests : \n")
         # for element with future contests
         index = elements[1].select('tbody tr')
         for tags in index:
             td = tags.select("td")
-            competitions.write('\nCompetion code : '+td[0].getText()+'\nContest Name: '+td[1].getText()+'\nStart time: '+td[2].getText()+'\nEnd time '+td[3].getText()+'\n\n')
-    
+            competitions.write('\nCompetion code : '+td[0].getText()+'\nContest Name: '+td[1].getText()+'\nStart time: '+td[2].getText()+'\nEnd time '+td[3].getText())
+            competitions.write("\n--------------------------------------------------------------------------------------\n")
+    elif x == 'Codeforces':
+        elements =soup.select('.datatable')
+        competitions.write("Current or upcoming contests : \n")
+        index = elements[0].select('tr[data-contestid]')
+        for tags in index :
+            td = tags.select("td")
+            competitions.write('\nContest name : '+td[0].contents[0]+'\nStart time : '+ td[2].getText()+'\nDuration : '+td[3].getText())
+            competitions.write("\n--------------------------------------------------------------------------------------\n")
+
 competitions.close()
 
 
