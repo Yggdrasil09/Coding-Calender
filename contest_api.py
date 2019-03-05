91ea4eb228039c431593809fb5906412dd6f35c1
Yggdrasil09 <karthik99th.tnk@gmail.com>
2019-03-05 18:10:51 +0530

    added the data fetch from an api

--- contest_api.py
+++ contest_api.py
@@ -1,8 +1,63 @@
 import requests,json
 
-response = requests.get("https://clist.by:443/api/v1/contest/?username=Karthikp99&api_key=54330957009d28b8a24eb8b7cb48dde28ae402da")
+response = requests.get("http://challengehuntapp.appspot.com/v2")
+    
+
+#writing to a file named competitions
+
+competitions=open('competitions2.txt','w')
+
+# print(response.status_code)
 
 data = response.json()
 
-data =data["objects"]
+#data is a list of contests
 
+list1={
+    'projecteuler.net':[],
+    'hackerrank.com':[],
+    'codechef.com':[],
+    'hackerearth':[],
+    'google.com/codejam':[],
+    'codeforces.com':[],
+    'codeforces.com/gyms':[],
+    'kaggle.com':[],
+    'topcoder':[],
+    'facebook.com/hackercup':[]
+}
+
+list2={
+    'projecteuler.net':[],
+    'hackerrank.com':[],
+    'codechef.com':[],
+    'hackerearth':[],
+    'google.com/codejam':[],
+    'codeforces.com':[],
+    'codeforces.com/gyms':[],
+    'kaggle.com':[],
+    'topcoder':[],
+    'facebook.com/hackercup':[]
+}
+
+active = data['active']
+pending = data["pending"]
+
+for items in active:
+    if items['host_name'] in list1:
+        list1[items['host_name']].append(items)
+
+for items in pending:
+    if items['host_name'] in list2:
+        list2[items['host_name']].append(items)
+
+for contests in list1 :
+    competitions.write('\n\n****\t'+contests+'\t****\n\n Active Contests')
+    if(len(list1[contests])>0):
+        for items in list1[contests]:
+            competitions.write('\n\nContest name : '+items['contest_name']+'\nDuration : '+items['duration']+'\nEnd Time : '+items['end']+'\n')
+            competitions.write("----------------------------------------------------------------------------------------")
+    competitions.write('\n Pending Contests')
+    if(len(list2[contests])>0):
+        for items in list2[contests]:
+            competitions.write('\n\nContest name : '+items['contest_name']+'\nDuration : '+items['duration']+'\nStart Time : '+items['start']+'\n')
+            competitions.write("----------------------------------------------------------------------------------------")
